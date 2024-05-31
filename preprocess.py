19babf4e68c92bbda272eb278aa82c305fa9adc8
DavidYu0222 <tisnesdavid@gmail.com>
2024-06-01 00:44:40 +0800

    Update preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -13,70 +13,61 @@ os.makedirs(output_folder_val, exist_ok=True)
 
 # Dictionary to store filenames by age
 age_dict = defaultdict(list)
-male_files = []
-female_files = []
+race_dict = defaultdict(list)
+gender_dict = defaultdict(list)
+
+Gender = {
+    '0': 'male',
+    '1': 'female'
+}
+
+# Race = {
+#     '0': 'White',
+#     '1': 'Black',
+#     '2': 'Asian',
+#     '3': 'Indian',
+#     '4': 'Other'
+# }
 
 # Process each file in the input folder
 for filename in os.listdir(input_folder):
     if filename.endswith('.jpg'):
-        # Extract the age from the filename
+        # Extract the age, gender, and race from the filename
         parts = filename.split('_')
         age = parts[0]
         gender = parts[1]
-        # Add the filename to the list of the corresponding age
+        # race = parts[2]
+        
+        # Add the filename to the list of the corresponding age, gender, and race
         age_dict[age].append(filename)
-        if gender == '0':  # Male
-            male_files.append(filename)
-        elif gender == '1':  # Female
-            female_files.append(filename)
+        gender_dict[Gender.get(gender)].append(filename)
+        # race_dict[Race.get(race)].append(filename)
 
-# Split the data and write the filenames to the corresponding age text files
-for age, files in age_dict.items():
-    random.shuffle(files)  # Shuffle the list of files for each age to ensure random split
-    split_index = int(0.8 * len(files))  # 80% for training, 20% for validation
-    train_files = files[:split_index]
-    val_files = files[split_index:]
+# Split the data and write the filenames to the corresponding text files
+def split_and_write_files(data_dict, output_folder):
+    for category, files in data_dict.items():
+        random.shuffle(files)  # Shuffle the list of files for each category to ensure random split
+        split_index = int(0.8 * len(files))  # 80% for training, 20% for validation
+        train_files = files[:split_index]
+        val_files = files[split_index:]
 
-    # Write training files
-    with open(os.path.join(output_folder_train, f'{age}.txt'), 'w') as f:
-        for file in train_files:
-            f.write(file + '\n')
+        # Write training files
+        with open(os.path.join(output_folder_train, f'{category}.txt'), 'w') as f:
+            for file in train_files:
+                f.write(file + '\n')
 
-    # Write validation files
-    with open(os.path.join(output_folder_val, f'{age}.txt'), 'w') as f:
-        for file in val_files:
-            f.write(file + '\n')
+        # Write validation files
+        with open(os.path.join(output_folder_val, f'{category}.txt'), 'w') as f:
+            for file in val_files:
+                f.write(file + '\n')
 
-# Shuffle the lists to ensure random split
-random.shuffle(male_files)
-random.shuffle(female_files)
+split_and_write_files(age_dict, output_folder_train)
+split_and_write_files(age_dict, output_folder_val)
 
-# Split the data
-split_index_male = int(0.8 * len(male_files))  # 80% for training, 20% for validation
-split_index_female = int(0.8 * len(female_files))
+split_and_write_files(gender_dict, output_folder_train)
+split_and_write_files(gender_dict, output_folder_val)
 
-train_male_files, val_male_files = male_files[:split_index_male], male_files[split_index_male:]
-train_female_files, val_female_files = female_files[:split_index_female], female_files[split_index_female:]
-
-# Write male training files
-with open(os.path.join(output_folder_train, 'male.txt'), 'w') as f:
-    for file in train_male_files:
-        f.write(file + '\n')
-
-# Write female training files
-with open(os.path.join(output_folder_train, 'female.txt'), 'w') as f:
-    for file in train_female_files:
-        f.write(file + '\n')
-
-# Write male validation files
-with open(os.path.join(output_folder_val, 'male.txt'), 'w') as f:
-    for file in val_male_files:
-        f.write(file + '\n')
-
-# Write female validation files
-with open(os.path.join(output_folder_val, 'female.txt'), 'w') as f:
-    for file in val_female_files:
-        f.write(file + '\n')
-
+# split_and_write_files(race_dict, output_folder_train)
+# split_and_write_files(race_dict, output_folder_val)
 
 print("Train and validation text files have been generated in the 'output/train' and 'output/val' folders.")
