f39603e36777cb08ea03d54e34250e4f93fc94a8
DavidYu0222 <tisnesdavid@gmail.com>
2024-05-31 13:58:43 +0800

    Update test.py

--- test.py
+++ test.py
@@ -3,26 +3,36 @@ import tensorflow as tf
 from tensorflow.keras.preprocessing import image
 import numpy as np
 
-# Load the trained model
-model = tf.keras.models.load_model('face_classification_model.h5')
+# Load the trained models
+gender_model = tf.keras.models.load_model('face_classification_gender_model.h5')
+age_model = tf.keras.models.load_model('face_classification_age_model.h5')
+
+# Compile the models to ensure metrics are available
+# gender_model.compile(optimizer='SGD', loss='binary_crossentropy', metrics=['accuracy'])
+gender_model.compile(optimizer='adam', loss='mean_squared_error', metrics=['mae'])
+age_model.compile(optimizer='adam', loss='mean_squared_error', metrics=['mae'])
 
 # Function to preprocess input photo
-def preprocess_image(image_path, target_size=(178, 218)):
+def preprocess_image(image_path, target_size=(200,200)):
     img = image.load_img(image_path, target_size=target_size)
     img_array = image.img_to_array(img)
     img_array = np.expand_dims(img_array, axis=0)
+    img_array = img_array / 255.0  # Normalize the image
     return img_array
 
-# Function to classify input photo
-def classify_photo(image_path):
-    img = preprocess_image(image_path)
-    prediction = model.predict(img)
-    # Assuming binary classification (e.g., gender), use threshold to determine label
-    if prediction >= 0.5:
-        label = "Female"
-    else:
-        label = "Male"
-    return label
+# Function to classify input photo for gender and predict age
+def classify_and_predict_age(image_path):
+    img1 = preprocess_image(image_path,(200,200))
+    # Gender prediction
+    gender_prediction = gender_model.predict(img1)
+    gender_label = "Female" if gender_prediction[0][0] >= 0.5 else "Male"
+    
+    img2 = preprocess_image(image_path,(200,200))
+    # Age prediction
+    age_prediction = age_model.predict(img2)
+    age_label = int(age_prediction[0][0])
+    
+    return gender_label, age_label
 
 # Function to classify all photos in a folder
 def classify_photos_in_folder(folder_path):
@@ -36,12 +46,12 @@ def classify_photos_in_folder(folder_path):
             # Get the full path of the file
             file_path = os.path.join(folder_path, file)
             
-            # Classify the photo
-            predicted_label = classify_photo(file_path)
+            # Classify the photo and predict age
+            gender_label, age_label = classify_and_predict_age(file_path)
             
-            # Print the predicted label
-            print(f"File: {file}, Predicted label: {predicted_label}")
+            # Print the predicted labels
+            print(f"File: {file}, Predicted Gender: {gender_label}, Predicted Age: {age_label}")
 
 # Example usage
-input_folder_path = 'resized_images'
+input_folder_path = 'crop_input'
 classify_photos_in_folder(input_folder_path)
