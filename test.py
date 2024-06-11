7c5ebb4dbea1d9068296caa1905a4e00b7d7e474
DavidYu0222 <tisnesdavid@gmail.com>
2024-06-11 12:50:05 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,16 +1,15 @@
 import os
+os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
 import tensorflow as tf
 from tensorflow.keras.preprocessing import image
 import numpy as np
+from Music_recommend import spotify_api
+
+current_year = 2024
 
 # Load the trained models
 gender_model = tf.keras.models.load_model('face_classification_gender_model.h5')
 age_model = tf.keras.models.load_model('face_classification_age_model.h5')
-
-# Compile the models to ensure metrics are available
-# gender_model.compile(optimizer='SGD', loss='binary_crossentropy', metrics=['accuracy'])
-gender_model.compile(optimizer='adam', loss='mean_squared_error', metrics=['mae'])
-age_model.compile(optimizer='adam', loss='mean_squared_error', metrics=['mae'])
 
 # Function to preprocess input photo
 def preprocess_image(image_path, target_size=(200,200)):
@@ -22,14 +21,13 @@ def preprocess_image(image_path, target_size=(200,200)):
 
 # Function to classify input photo for gender and predict age
 def classify_and_predict_age(image_path):
-    img1 = preprocess_image(image_path,(200,200))
+    img = preprocess_image(image_path)
     # Gender prediction
-    gender_prediction = gender_model.predict(img1)
+    gender_prediction = gender_model.predict(img)
     gender_label = "Female" if gender_prediction[0][0] >= 0.5 else "Male"
     
-    img2 = preprocess_image(image_path,(200,200))
     # Age prediction
-    age_prediction = age_model.predict(img2)
+    age_prediction = age_model.predict(img)
     age_label = int(age_prediction[0][0])
     
     return gender_label, age_label
@@ -51,6 +49,17 @@ def classify_photos_in_folder(folder_path):
             
             # Print the predicted labels
             print(f"File: {file}, Predicted Gender: {gender_label}, Predicted Age: {age_label}")
+            year = current_year - age_label + min(age_label, 18)
+            df_all_songs = spotify_api.fetch_desired_year_songs(year)
+            
+            # Print song names and singers
+            if not df_all_songs.empty:
+                # Randomly select 5 songs
+                random_songs = df_all_songs.sample(n=3)
+                for _, song in random_songs.iterrows():
+                    print(f"Title: {song['title']}, Artist: {song['artist']}\nSong URL: {song['spotify_url']}")
+            else:
+                print(f"No songs found for year {year}")
 
 # Example usage
 input_folder_path = 'crop_input'
